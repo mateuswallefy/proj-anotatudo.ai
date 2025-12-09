d238933dd46d1a6907f9854a6a0023b9224b505a
mateuswallefy <matheus.wallefy@gmail.com>
2025-12-09 16:31:55 +0000

    errou e colocou o dashboard antigo

--- client/src/components/dashboard/DashboardContainer.tsx
+++ client/src/components/dashboard/DashboardContainer.tsx
@@ -1,6 +1,4 @@
 import { ReactNode } from "react";
-import { DashboardSidebar } from "./DashboardSidebar";
-import { cn } from "@/lib/utils";
 
 interface DashboardContainerProps {
   children: ReactNode;
@@ -9,14 +7,10 @@ interface DashboardContainerProps {
 export function DashboardContainer({ children }: DashboardContainerProps) {
   return (
     <div className="min-h-screen bg-background">
-      <DashboardSidebar />
-      
       {/* Main Content */}
-      <div className="lg:pl-64">
-        <main className="p-4 md:p-6 lg:p-8 max-w-7xl mx-auto">
-          {children}
-        </main>
-      </div>
+      <main className="p-4 md:p-6 lg:p-8 max-w-7xl mx-auto">
+        {children}
+      </main>
     </div>
   );
 }
